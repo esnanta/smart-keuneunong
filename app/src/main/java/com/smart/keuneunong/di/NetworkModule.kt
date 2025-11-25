7c83d367d95992d5cc5e8a656e1bc6b9881897df
esnanta <ombakrinai@gmail.com>
2025-11-25 23:09:03 +0700

    remove module user

--- app/src/main/java/com/smart/keuneunong/di/NetworkModule.kt
+++ app/src/main/java/com/smart/keuneunong/di/NetworkModule.kt
@@ -3,8 +3,6 @@ package com.smart.keuneunong.di
 import com.fasterxml.jackson.dataformat.xml.XmlMapper
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
 import com.smart.keuneunong.BuildConfig
-import com.smart.keuneunong.data.network.DetailsApi
-import com.smart.keuneunong.data.network.UsersApi
 import com.smart.keuneunong.data.network.WeatherApi
 import dagger.Module
 import dagger.Provides
@@ -14,7 +12,6 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.jackson.JacksonConverterFactory
-import retrofit2.converter.moshi.MoshiConverterFactory
 import javax.inject.Singleton
 
 @Module
@@ -47,16 +44,6 @@ object NetworkModule {
         .baseUrl("https://data.bmkg.go.id/")
         .client(okHttpClient)
         .build()
-
-    @Provides
-    @Singleton
-    fun provideApiService(@GithubApiRetrofit retrofit: Retrofit): UsersApi =
-        retrofit.create(UsersApi::class.java)
-
-    @Provides
-    @Singleton
-    fun provideUserDetailsService(@GithubApiRetrofit retrofit: Retrofit): DetailsApi =
-        retrofit.create(DetailsApi::class.java)
 
     @Provides
     @Singleton
--- app/src/main/java/com/smart/keuneunong/ui/users/UsersUiState.kt
+++
@@ -1,8 +0,0 @@
-package com.smart.keuneunong.ui.users
-
-import com.smart.keuneunong.domain.User
-
-data class UsersUiState(
-    val list: List<User> = listOf(),
-    val offline: Boolean = false
-)
